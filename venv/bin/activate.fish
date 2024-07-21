608f646ba0db6f62b03cdf7f8fd0a5f432803ad9
Ava Isaacson <avajeano@gmail.com>
2024-07-21 16:19:50 -0400

    requirements update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/avaisaacson/Desktop/Code/capstone-1-restaurants-app/venv"
+set -gx VIRTUAL_ENV "/Users/avaisaacson/Desktop/Code/GoodFeed/GoodFeed/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
